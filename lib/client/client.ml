f7ed35ec21766240f27fb6a41ce4169880c0b84e
Pierre-Emmanuel Wulfman <pierre.emmanuel.wulfman@gmail.com>
2024-03-19 18:25:11 +0100

    basic client server implementation

--- lib/client/client.ml
+++ lib/client/client.ml
@@ -1,1 +1,18 @@
-let run host port = Printf.printf "Connecting to %s:%d\n" host port
+open Eio
+
+let connet ~net host port sw: _ Net.stream_socket_ty Std.r =
+  let host =
+    if host = "localhost" then
+      Net.Ipaddr.V4.loopback
+    else Net.Ipaddr.of_raw host
+  in
+  Net.connect ~sw net (`Tcp (host, port))
+
+
+let run_eio host port env : unit =
+  let net = Stdenv.net env in
+  Switch.run ~name:"client" @@ fun sw ->
+    let flow = connet ~net host port sw in
+    Eio.Flow.copy flow (Stdenv.stdout env)
+
+let run host port = Eio_main.run (run_eio host port)
--- lib/server/server.ml
+++ lib/server/server.ml
@@ -1,1 +1,32 @@
-let run host port = Printf.printf "Spawning server on %s:%d\n" host port
+open Eio
+
+
+let handle_connect : _ Net.connection_handler = fun socket stream ->
+  print_endline "New connection";
+  Format.printf "Socket: %a\n" Net.Sockaddr.pp stream ;
+  Format.print_flush();
+  Flow.copy_string "Hello, world!" socket;
+  Flow.close socket
+
+
+let listening_soket (host : string) (port : int) (net : _ Std.r) =
+  (* TODO : host and port sanitation *)
+  let host =
+    if host = "localhost" then
+      Net.Ipaddr.V4.loopback
+    else Net.Ipaddr.of_raw host
+  in
+  Net.listen ~backlog:10
+  net (`Tcp (host, port))
+
+let run_eio host port env =
+  let () = Printf.printf "Spawning server on %s:%d\n" host port in
+  let net = Stdenv.net env in
+  Switch.run ( fun sw ->
+  Net.run_server ~on_error:(fun e -> Printf.printf "Error: %s\n" (Printexc.to_string e))
+    (listening_soket host port net ~sw)
+    (handle_connect)
+  )
+
+let run host port =
+  Eio_main.run (run_eio host port)
